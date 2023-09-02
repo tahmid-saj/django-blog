5f6b9ad94e099915aa4a6367fe8a1b2525e39442
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 15:34:37 -0400

    converting to class views

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,23 @@
 from datetime import date
+from typing import Any, Dict
+from django.db.models.query import QuerySet
 
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Post
+
+class StartingPageView(ListView):
+  template_name = "blog/index.html"
+  model = Post
+  ordering = ["-date"]
+  context_object_name = "posts"
+  
+  def get_queryset(self):
+    queryset = super().get_queryset()
+    data = queryset[:3]
+    
+    return data
 
 all_posts = [
 ]
@@ -19,6 +34,22 @@ def starting_page(request):
   return render(request, "blog/index.html", {
     "posts": latest_posts
   })
+  
+class AllPostsView(ListView):
+  template_name = "blog/all-posts.html"
+  model = Post
+  ordering = ["-date"]
+  context_object_name = "all_posts"
+  
+class SinglePostView(DetailView):
+  template_name = "blog/post-detail.html"
+  model = Post
+  
+  def get_context_data(self, **kwargs: Any):
+    context = super().get_context_data(**kwargs)
+    context["post_tags"] = self.object.tags.all()
+    
+    return context
 
 def posts(request):
   all_posts = Post.objects.all().order_by("-date")
