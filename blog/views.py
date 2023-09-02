df3aaec5ff09c482a1dd7b29284eea2e0f530600
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 18:13:06 -0400

    comment form submission

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,10 @@ from typing import Any, Dict
 from django.db.models.query import QuerySet
 
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -42,16 +45,45 @@ class AllPostsView(ListView):
   ordering = ["-date"]
   context_object_name = "all_posts"
   
-class SinglePostView(DetailView):
-  template_name = "blog/post-detail.html"
-  model = Post
+class SinglePostView(View):
+  # template_name = "blog/post-detail.html"
+  # model = Post
   
-  def get_context_data(self, **kwargs: Any):
-    context = super().get_context_data(**kwargs)
-    context["post_tags"] = self.object.tags.all()
-    context["comment_form"] = CommentForm()
+  # def get_context_data(self, **kwargs: Any):
+  #   context = super().get_context_data(**kwargs)
+  #   context["post_tags"] = self.object.tags.all()
+  #   context["comment_form"] = CommentForm()
     
-    return context
+  #   return context
+  
+  def get(self, request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+      "post": post,
+      "post_tags": post.tags.all(),
+      "comment_form": CommentForm()
+    }
+    
+    return render(request, "blog/post-detail.html", context=context)
+    
+  def post(self, request, slug):
+    post = Post.objects.get(slug=slug)
+    comment_form = CommentForm(request.POST)
+    
+    if comment_form.is_valid():
+      comment = comment_form.save(commit=False)
+      comment.post = post
+      comment.save()
+      
+      return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+    
+    context = {
+      "post": post,
+      "post_tags": post.tags.all(),
+      "comment_form": comment_form
+    }
+    
+    return render(request, "blog/post-detail.html", context=context)
 
 def posts(request):
   all_posts = Post.objects.all().order_by("-date")
