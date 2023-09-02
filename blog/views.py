0083d95bbe925a346851e045023c193d7d7b93d0
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 14:38:50 -0400

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,35 @@
+from datetime import date
+
+from django.shortcuts import render, get_object_or_404
+
+from .models import Post
+
+all_posts = [
+]
+
+def get_date(post):
+  return post["date"]
+
+# Create your views here.
+
+def starting_page(request):
+  latest_posts = Post.objects.all().order_by("-date")[:3] # order by desc order
+  # sorted_posts = sorted(all_posts, key=get_date)
+  # latest_posts = sorted_posts[-3:]
+  return render(request, "blog/index.html", {
+    "posts": latest_posts
+  })
+
+def posts(request):
+  all_posts = Post.objects.all().order_by("-date")
+  return render(request, "blog/all-posts.html", {
+    "all_posts": all_posts
+  })
+
+def post_detail(request, slug):
+  identified_post = get_object_or_404(Post, slug=slug)
+
+  return render(request, "blog/post-detail.html", {
+    "post": identified_post,
+    "post_tags": identified_post.tags.all()
+  })
