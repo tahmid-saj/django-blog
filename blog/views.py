7d3c442b6707b0caf693bec98befe1b9de0ff58b
tahmid-saj <tahmid.saj@gmail.com>
2023-09-02 23:55:06 -0400

    Outputting comments

--- blog/views.py
+++ blog/views.py
@@ -61,7 +61,8 @@ class SinglePostView(View):
     context = {
       "post": post,
       "post_tags": post.tags.all(),
-      "comment_form": CommentForm()
+      "comment_form": CommentForm(),
+      "comments": post.comments.all().order_by("-id")
     }
     
     return render(request, "blog/post-detail.html", context=context)
@@ -80,7 +81,8 @@ class SinglePostView(View):
     context = {
       "post": post,
       "post_tags": post.tags.all(),
-      "comment_form": comment_form
+      "comment_form": comment_form,
+      "comments": post.comments.all().order_by("-id")
     }
     
     return render(request, "blog/post-detail.html", context=context)
